47f89c02bda6c0fedfcc58c0a240b29761c1cea9
DaRoMi2020 <daniel.r.milman@gmail.com>
2020-06-22 18:57:46 -0700

    06/22/2020

--- Enchantment_function.sql
+++ Enchantment_function.sql
@@ -1,4 +1,3 @@
---Main branch of the enchantment function
 
 CREATE FUNCTION enchantment_function (
 	enchantments_limit_v integer, 
@@ -6,23 +5,21 @@ CREATE FUNCTION enchantment_function (
 	format_v em_format, 
 	status_v em_status, 
 	color_v TEXT,
-	enchantments_types_v TEXT,
 	enchantments_sub_exclude TEXT DEFAULT NULL,
 	enchantments_sub_include TEXT DEFAULT NULL,
 	enchantments_super_exclude TEXT DEFAULT NULL,
 	enchantments_super_include TEXT DEFAULT NULL)
 
 
-RETURNS TABLE (c_name_t TEXT,
-	c_id_t integer,
-	c_color_t TEXT,
-	c_rarity_t em_rarity,
-	c_types_t TEXT,
-	c_subtypes_t TEXT,
-	c_supertypes_t TEXT,
-	l_id_t integer,
-	l_form_t em_format,
-	l_stat_t em_status
+RETURNS TABLE (card_name TEXT,
+	card_id integer,
+	card_colors TEXT,
+	card_rarity em_rarity,
+	card_types TEXT,
+	card_subtypes TEXT,
+	card_supertypes TEXT,
+	card_format em_format,
+	card_format_status em_status
 	)
 
 AS $T$
@@ -39,8 +36,7 @@ cards.colors,
 cards.rarity,
 cards.types,
 cards.subtypes,
-cards.supertypes,
-legalities.id, 
+cards.supertypes
 legalities.format, 
 legalities.status 
 FROM cards 
@@ -50,17 +46,19 @@ ON cards.uuid = legalities.uuid
 WHERE 
 cards.colors::TEXT ILIKE color_v::TEXT AND
 cards.rarity = enchantments_rarity_v::em_rarity AND
-cards.types::TEXT ILIKE enchantments_types_v::TEXT AND
 cards.types::TEXT NOT ILIKE '%Land%' AND
 cards.types::TEXT NOT ILIKE '%Creature%' AND
+legalities.format = format_v::em_format AND 
+legalities.status = status_v::em_status AND
 ((enchantments_sub_exclude IS NULL AND cards.subtypes::TEXT IS NULL) OR 
 	(enchantments_sub_exclude IS NOT NULL AND cards.subtypes::TEXT ILIKE enchantments_sub_include) OR 
 	(enchantments_sub_exclude IS NULL AND cards.subtypes::TEXT ILIKE enchantments_sub_include)) AND
 ((enchantments_super_exclude IS NULL AND cards.supertypes::TEXT IS NULL) OR 
 	(enchantments_super_exclude IS NOT NULL AND cards.supertypes::TEXT ILIKE enchantments_super_include) OR 
-	(enchantments_super_exclude IS NULL AND cards.supertypes::TEXT ILIKE enchantments_super_include)) AND
-legalities.format = format_v::em_format AND 
-legalities.status = status_v::em_status
+	(enchantments_super_exclude IS NULL AND cards.supertypes::TEXT ILIKE enchantments_super_include))
+
+
+
 ORDER BY (cards.name))
 
 SELECT * 
@@ -69,3 +67,19 @@ ORDER BY random()
 LIMIT enchantments_limit_v::integer;
 
 END; $T$ LANGUAGE 'plpgsql';
+
+
+--function testing
+
+--
+
+--
+
+--
+
+--
+
+--
+
+--
+
