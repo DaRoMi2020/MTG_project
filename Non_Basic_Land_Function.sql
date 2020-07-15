c39dbdedf8eedec83cf26e06a34856953da00524
DaRoMi2020 <daniel.r.milman@gmail.com>
2020-07-15 16:39:50 -0700

    improved type logic. focus on improving removing basic supertypes

--- Non_Basic_Land_Function.sql
+++ Non_Basic_Land_Function.sql
@@ -1,16 +1,17 @@
 
 -- Non-Basic Land 
 
-
 CREATE FUNCTION non_basic_land_function(
 	non_basic_land_limit_v INTEGER, 
 	non_basic_land_rarity_v em_rarity,
 	format_v em_format, 
 	status_v em_status,
-	non_basic_color_identity_exclude TEXT DEFAULT NULL,
-	non_basic_color_identity_include TEXT [] DEFAULT array[['B'], ['G'], ['R'], ['U'], ['W']],
+	non_basic_land_types_exclude TEXT DEFAULT NULL,
+	non_basic_land_types_include TEXT DEFAULT NULL,
 	non_basic_land_super_exclude TEXT DEFAULT NULL,
-	non_basic_land_super_include TEXT [] DEFAULT array[['Legendary'],['Snow']])
+	non_basic_land_super_include TEXT [] DEFAULT ARRAY[['Legendary'],['Snow']],
+	non_basic_color_identity_exclude TEXT DEFAULT NULL,
+	non_basic_color_identity_include TEXT [] DEFAULT ARRAY[['B'], ['G'], ['R'], ['U'], ['W']])
 
 
 
@@ -48,16 +49,19 @@ WHERE
 	cards.rarity = non_basic_land_rarity_v::em_rarity AND
 	legalities.format = format_v::em_format AND 
 	legalities.status = status_v::em_status AND
-	((non_basic_color_identity_exclude::TEXT IS NULL AND cards.coloridentity::TEXT IS NULL OR cards.coloridentity::TEXT = ANY (non_basic_color_identity_include::TEXT[])) OR -- include all choosen including null
-		(non_basic_color_identity_exclude::TEXT IS NULL AND cards.coloridentity::TEXT IS NULL AND cards.coloridentity::TEXT[] IS NULL) OR -- excludes non-null color identities
-		(non_basic_color_identity_exclude::TEXT IS NOT NULL AND cards.coloridentity::TEXT IS NOT NULL AND cards.coloridentity::TEXT ~* ANY (non_basic_color_identity_include::TEXT[]))) AND-- exclude nulls
 	cards.types::TEXT NOT ILIKE '%Creature%' AND
-	cards.types::TEXT ILIKE '%Land%' AND
-	cards.supertypes::TEXT NOT ILIKE '%Basic%' AND
-	((non_basic_land_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL OR cards.supertypes::TEXT ~* ANY (non_basic_land_super_include::TEXT[])) OR --include all choosen including null
-		(non_basic_land_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL AND non_basic_land_super_include::TEXT[] IS NULL) OR --Exclude supertypes not null
-		(non_basic_land_super_exclude::TEXT IS NOT NULL AND cards.supertypes::TEXT IS NOT NULL AND cards.supertypes::TEXT ~* ANY (non_basic_land_super_include::TEXT[]))) --include all choosen and exclude nulls 
 
+((non_basic_land_types_exclude::TEXT IS NULL AND cards.types::TEXT ILIKE '%Land%' AND non_basic_land_types_include::TEXT IS NULL) OR --include all creatures types depending on subtype options
+	(non_basic_land_types_exclude::TEXT IS NOT NULL AND cards.types::TEXT ILIKE 'Land' AND non_basic_land_types_include::TEXT IS NULL) OR -- default excludes non-basic creature types
+	(non_basic_land_types_exclude::TEXT IS NOT NULL AND cards.types::TEXT ILIKE '%Land%' AND cards.types::TEXT  ILIKE non_basic_land_types_include::TEXT)) AND -- includes basic creature types and added types
+
+	((non_basic_land_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL OR cards.supertypes::TEXT ~* ANY (non_basic_land_super_include::TEXT[]) AND cards.supertypes::TEXT !~* ANY (array[['Basic'], ['Basic,Snow']])) OR --include all choosen including null
+		(non_basic_land_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL AND non_basic_land_super_include::TEXT[] IS NULL) OR --Exclude supertypes not null
+		(non_basic_land_super_exclude::TEXT IS NOT NULL AND cards.supertypes::TEXT IS NOT NULL AND cards.supertypes::TEXT ~* ANY (non_basic_land_super_include::TEXT[]) AND cards.supertypes::TEXT !~* ANY (array[['Basic'], ['Basic,Snow']]))) AND --include all choosen and exclude nulls 
+
+	((non_basic_color_identity_exclude::TEXT IS NULL AND cards.coloridentity::TEXT IS NULL OR cards.coloridentity::TEXT = ANY (non_basic_color_identity_include::TEXT [])) OR -- include all choosen including null
+		(non_basic_color_identity_exclude::TEXT IS NULL AND cards.coloridentity::TEXT IS NULL AND non_basic_color_identity_include::TEXT[] IS NULL) OR -- excludes non-null color identities
+		(non_basic_color_identity_exclude::TEXT IS NOT NULL AND cards.coloridentity::TEXT IS NOT NULL AND cards.coloridentity::TEXT = ANY (non_basic_color_identity_include::TEXT []))) -- exclude null
 
 ORDER BY (cards."name"))
 
@@ -68,22 +72,52 @@ LIMIT non_basic_land_limit_v::integer;
 
 END; $T$ LANGUAGE 'plpgsql';
 
-SELECT DISTINCT ON (subtypes) subtypes, supertypes, types FROM cards WHERE types ILIKE '%Land%' AND supertypes NOT ILIKE '%Basic%';
 
-SELECT * FROM non_basic_land_function(10, 'rare', 'legacy', 'Legal');
-
-SELECT DISTINCT ON (types) types FROM cards WHERE types ILIKE '%Land%' ;
-
-SELECT DISTINCT ON (subtypes) subtypes, types FROM cards WHERE types ILIKE '%Land%' ;
+--------------------------------------
 
 
-SELECT DISTINCT ON (coloridentity) coloridentity, types FROM cards WHERE types ILIKE 'Land' AND coloridentity::TEXT ~* ALL (array[['B'], ['G'], ['U'], ['W'], ['R'], [NULL]]);
+-- Function Testing
 
-SELECT DISTINCT ON (coloridentity) coloridentity, types FROM cards WHERE types ILIKE 'Land' AND coloridentity::TEXT ~* ANY (array['B', 'G', 'U', 'W', 'R', NULL]);
+SELECT * FROM non_basic_land_function(1000, 'common', 'legacy', 'Legal');
 
-SELECT DISTINCT ON (coloridentity) coloridentity, types FROM cards WHERE types ILIKE 'Land' AND coloridentity::TEXT ~* ANY (array['{"B"}', '{"G"}', '{"U"}', '{"W"}', '{"R"}', '{NULL}']);
+SELECT * FROM non_basic_land_function(1000, 'rare', 'legacy', 'Legal', NULL, NULL);
 
-SELECT DISTINCT ON (coloridentity) coloridentity, types FROM cards WHERE types ILIKE 'Land' AND coloridentity::TEXT = ANY ('{"B"}', NULL);
+SELECT * FROM non_basic_land_function(1000, 'rare', 'legacy', 'Legal', NULL, NULL, NULL,  ARRAY[['Legendary'],['Snow']]);
 
+SELECT * FROM non_basic_land_function(1000, 'rare', 'legacy', 'Legal', NULL, NULL, NULL,  ARRAY[['Legendary'],['Snow']], NULL, ARRAY[['B'], ['G'], ['R'], ['U'], ['W']]);
+
+-- Includes all non-Basic lands types, includes all supertypes, includes colored mana production, default
+
+SELECT * FROM non_basic_land_function(1000, 'common', 'legacy', 'Legal', 'Exclude', '%Artifact%');
+
+-- Includes only artifact non-Basic land type, excludes all non-null supertypes based on data, includes colored mana production
+
+SELECT * FROM non_basic_land_function(1000, 'rare', 'legacy', 'Legal', NULL, NULL, NULL, NULL);
+
+-- Includes all non-Basic lands types, excludes non-nulls supertypes, includes colored mana production
+
+SELECT * FROM non_basic_land_function(1000, 'rare', 'legacy', 'Legal', NULL, NULL, 'Exclude', ARRAY[['Legendary'],['Snow']]);
+
+-- Includes all non-Basic lands types, excludes nulls supertypes, includes colored mana production
+
+SELECT * FROM non_basic_land_function(1000, 'uncommon', 'legacy', 'Legal', NULL, NULL, NULL,  ARRAY[['Legendary'],['Snow']], NULL, NULL);
+
+-- Includes all non-Basic lands types, includes all supertypes, excludes colored mana production
+
+SELECT * FROM non_basic_land_function(1000, 'uncommon', 'legacy', 'Legal', NULL, NULL, NULL,  ARRAY[['Legendary'],['Snow']], 'Exclude', ARRAY[['B'], ['G'], ['R'], ['U'], ['W']]);
+
+-- Includes all non-Basic lands types, includes all supertypes, excludes colorless mana production
+
+
+-----------------------------------
+
+
+-- Query Type, Subtypes, Supertypes
+
+SELECT DISTINCT ON (types) types, subtypes, supertypes FROM cards  WHERE types ILIKE '%Land%';
+
+SELECT DISTINCT ON (subtypes) subtypes, supertypes, types FROM cards WHERE types ILIKE '%Land%';
+
+SELECT DISTINCT ON (supertypes) supertypes, subtypes, types FROM cards WHERE types ILIKE '%Land%' AND supertypes NOT ILIKE '%Basic%';
 
 
