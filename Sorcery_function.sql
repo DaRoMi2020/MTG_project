cb171bc9decc0b516bdfb5f368176ed1de8e9347
DaRoMi2020 <daniel.r.milman@gmail.com>
2020-07-17 14:17:47 -0700

    added rarity and color logic, minor type logic improvements, detailed comments on function options added

--- Sorcery_function.sql
+++ Sorcery_function.sql
@@ -1,15 +1,19 @@
---Sorcery function
+-- Sorceries function
 
 
 /**/
 
 
 CREATE FUNCTION sorceries_function (
-	sorceries_limit_v integer, 
-	sorceries_rarity_v em_rarity,
+	sorceries_limit_v integer,
 	format_v em_format, 
-	status_v em_status, 
-	color_v TEXT,
+	status_v em_status,
+	sorceries_rarity_floor em_rarity DEFAULT 'common',
+	sorceries_rarity_ceiling em_rarity DEFAULT 'mythic',
+	sorceries_colors_primary_exclude TEXT DEFAULT NULL,
+	sorceries_colors_primary_include TEXT [] DEFAULT array[['B'], ['G'], ['U'], ['W'], ['R']],
+	sorceries_colors_secondary TEXT [] DEFAULT NULL,
+	sorceries_colors_exclude_include TEXT DEFAULT 'Exclude',
 	sorceries_type_exclude TEXT DEFAULT NULL,
 	sorceries_type_include TEXT DEFAULT NULL,
 	sorceries_sub_exclude TEXT DEFAULT NULL,
@@ -50,22 +54,29 @@ LEFT OUTER JOIN legalities
 ON cards.uuid = legalities.uuid
 
 WHERE 
-	cards.colors::TEXT ILIKE color_v::TEXT AND
-	cards.rarity = sorceries_rarity_v::em_rarity AND
 	legalities.format = format_v::em_format AND 
 	legalities.status = status_v::em_status AND
 
-((sorceries_type_exclude::TEXT IS NULL AND cards.types::TEXT ILIKE '%Sorcery%' AND cards.types::TEXT ILIKE sorceries_type_include::TEXT IS NULL) OR -- Include or exclude tribal depending on subtype selection, 
-	(sorceries_type_exclude::TEXT IS NOT NULL AND cards.types::TEXT ILIKE 'Sorcery' AND sorceries_type_include::TEXT IS NULL) OR -- Exclude tribal
-	(sorceries_type_exclude::TEXT IS NULL AND cards.types::TEXT ILIKE '%Sorcery%' AND cards.types::TEXT ILIKE sorceries_type_include::TEXT)) AND -- only tribal
+	cards.rarity = ANY(enum_range(sorceries_rarity_floor::em_rarity, sorceries_rarity_ceiling::em_rarity)) AND
 
-((sorceries_sub_exclude::TEXT IS NULL AND cards.subtypes::TEXT IS NULL OR cards.subtypes::TEXT ~* ANY (sorceries_sub_include::TEXT[])) OR --include all choosen including null 
-	(sorceries_sub_exclude::TEXT IS NULL AND cards.subtypes::TEXT IS NULL AND sorceries_sub_include::TEXT[] IS NULL) OR --Exclude subtypes not null
-	(sorceries_sub_exclude::TEXT IS NOT NULL AND cards.subtypes::TEXT IS NOT NULL AND cards.subtypes::TEXT ~* ANY (sorceries_sub_include::TEXT[]))) AND --exclude nulls
+	((sorceries_colors_primary_exclude::TEXT IS NULL AND cards.colors::TEXT IS NULL OR cards.colors::TEXT ~* ANY (sorceries_colors_primary_include::TEXT[])) OR -- include all choosen including null
+		(sorceries_colors_primary_exclude::TEXT IS NULL AND cards.colors::TEXT IS NULL AND sorceries_colors_primary_include::TEXT[] IS NULL) OR -- excludes non-null colors
+		(sorceries_colors_primary_exclude::TEXT IS NOT NULL AND cards.colors::TEXT IS NOT NULL AND cards.colors::TEXT ~* ANY (sorceries_colors_primary_include::TEXT[]))) AND -- exclude null
 
-((sorceries_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL OR cards.supertypes::TEXT ILIKE sorceries_super_include::TEXT) OR --include legendary including null
-	(sorceries_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL AND sorceries_super_include::TEXT IS NULL) OR --Exclude supertypes not null
-	(sorceries_super_exclude::TEXT IS NOT NULL AND cards.supertypes::TEXT IS NOT NULL AND cards.supertypes::TEXT ILIKE sorceries_super_include::TEXT)) --include legendary and exclude nulls
+	((sorceries_colors_exclude_include::TEXT ILIKE 'Exclude' AND (sorceries_colors_secondary::TEXT[] IS NULL OR cards.colors::TEXT !~* ALL (sorceries_colors_secondary::TEXT[]))) OR
+		(sorceries_colors_exclude_include::TEXT ILIKE 'Include' AND (sorceries_colors_secondary::TEXT[] IS NULL OR cards.colors::TEXT ~* ANY (sorceries_colors_secondary::TEXT[])))) AND
+
+	((sorceries_type_exclude::TEXT IS NULL AND cards.types::TEXT ILIKE '%Sorcery%' AND cards.types::TEXT ILIKE sorceries_type_include::TEXT IS NULL) OR -- Include or exclude tribal depending on subtype selection, 
+		(sorceries_type_exclude::TEXT IS NOT NULL AND cards.types::TEXT ILIKE 'Sorcery' AND sorceries_type_include::TEXT IS NULL) OR -- Exclude tribal
+		(sorceries_type_exclude::TEXT IS NOT NULL AND cards.types::TEXT ILIKE '%Sorcery%' AND cards.types::TEXT ILIKE sorceries_type_include::TEXT)) AND -- only tribal
+
+	((sorceries_sub_exclude::TEXT IS NULL AND cards.subtypes::TEXT IS NULL OR cards.subtypes::TEXT ~* ANY (sorceries_sub_include::TEXT[])) OR --include all choosen including null 
+		(sorceries_sub_exclude::TEXT IS NULL AND cards.subtypes::TEXT IS NULL AND sorceries_sub_include::TEXT[] IS NULL) OR --Exclude subtypes not null
+		(sorceries_sub_exclude::TEXT IS NOT NULL AND cards.subtypes::TEXT IS NOT NULL AND cards.subtypes::TEXT ~* ANY (sorceries_sub_include::TEXT[]))) AND --exclude nulls
+
+	((sorceries_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL OR cards.supertypes::TEXT ILIKE sorceries_super_include::TEXT) OR --include legendary including null
+		(sorceries_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL AND sorceries_super_include::TEXT IS NULL) OR --Exclude supertypes not null
+		(sorceries_super_exclude::TEXT IS NOT NULL AND cards.supertypes::TEXT IS NOT NULL AND cards.supertypes::TEXT ILIKE sorceries_super_include::TEXT)) --include legendary and exclude nulls
 
 ORDER BY (cards."name"))
 
@@ -82,30 +93,122 @@ END; $T$ LANGUAGE 'plpgsql';
 
 -- Function Testing
 
-SELECT * FROM sorceries_function (1000, 'uncommon', 'legacy', 'Legal', 'G'); 
+-- Default
 
-SELECT * FROM sorceries_function (1000, 'uncommon', 'legacy', 'Legal', 'G', NULL, NULL, NULL, array[['Adventure'], ['Arcane'], ['Eldrazi'], ['Elemental'], ['Elf'], ['Giant'], 
-	['Goblin'], ['Merfolk'], ['Rogue']], NULL, 'Legendary');
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal');
 
--- includes all sorcery types, includes all subtypes, includes all supertypes (default)
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic');
 
-SELECT * FROM sorceries_function (1000, 'uncommon', 'legacy', 'Legal', 'B', NULL, '%Tribal%');
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude');
 
--- includes only tribal enchantments, including only tribal subtypes, includes all supertypes 
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL);
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Adventure'], ['Arcane'], ['Eldrazi'], ['Elemental'], ['Elf'], ['Giant'], ['Goblin'], ['Merfolk'], ['Rogue']]);
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Adventure'], ['Arcane'], ['Eldrazi'], ['Elemental'], ['Elf'], ['Giant'], ['Goblin'], ['Merfolk'], ['Rogue']], NULL, 'Legendary');
+
+-- 1000 random cards (not a default option), legacy format (not a default option), legal in format(not a default option), 
+-- Rarities between common and mythic rare, includes all colored and null sorceries, excludes no colored and null sorceries
+-- Includes all types, include all subtypes, includes all supertypes
 
 
-SELECT * FROM sorceries_function (1000, 'uncommon', 'legacy', 'Legal', 'B', NULL, NULL, 'include', array[['Goblin']]);
+-- Rarity options
 
--- Includes tribal type only based on including tribal subtype, including tribal subtype, includes supertypes 
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic');
+
+-- Default 
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'uncommon', 'uncommon');
+
+-- Only includes enchantments that are uncommon
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'rare');
+
+-- Only includes enchantments with rarities that are between common and rare excludes mythic rares
 
 
-SELECT * FROM sorceries_function (1000, 'rare', 'legacy', 'Legal', 'B', NULL, NULL, NULL, NULL, NULL, NULL);
+-- Exclusion mode
 
--- excludes tribal type only based on excludes tribal subtype, excludes all non-null subtype, excludes non-null supertype
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude');
 
-SELECT * FROM sorceries_function (1000, 'rare', 'legacy', 'Legal', 'B', NULL, NULL, NULL, NULL, 'include', 'Legendary');
+-- Default
 
--- Excludes tribal type based on excluding subtype, excludes all subtypes, includes only legendary supertypes 
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G']], NULL, 'Exclude');
+
+-- Every card is either a black, green, or colorless, might have other colors as secondary colors.
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', 'Exclude Nulls', array[['B'], ['G']], NULL, 'Exclude');
+
+-- Every card is either a black or green but not colorless, might have other colors as secondary colors.
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', 'Exclude Nulls', array[['B'], ['G']], array[['W'], ['R'], ['U']], 'Exclude');
+
+-- Every card is either a black and/or green but not colorless, excludes white, red, and blue.
+
+-- Inclusion mode
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B']], array[['G']], 'Include');
+
+-- every card must contain black and green, might have other colors as secondary colors.
+
+
+-- Types options
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL);
+
+-- Include all sorcery types
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', 'Exclude', NULL);
+
+-- Exclude tribal sorcery type
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', 'Include', '%Tribal%');
+
+-- Include only tribal sorcery type
+
+
+-- Subtypes
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Adventure'], ['Arcane'], ['Eldrazi'], ['Elemental'], ['Elf'], ['Giant'], ['Goblin'], ['Merfolk'], ['Rogue']]);
+
+-- Default
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Adventure'], ['Arcane']]);
+
+-- Includes all null subtypes and selected subtypes
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, NULL);
+
+-- Excludes non-null subtypes (also excludes tribal type)
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	'Exclude', array[['Adventure'], ['Arcane'], ['Eldrazi'], ['Elemental'], ['Elf'], ['Giant'], ['Goblin'], ['Merfolk'], ['Rogue']]);
+
+--Excludes null subtypes
+
+
+--Supertypes Options
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Adventure'], ['Arcane'], ['Eldrazi'], ['Elemental'], ['Elf'], ['Giant'], ['Goblin'], ['Merfolk'], ['Rogue']], NULL, 'Legendary');
+
+-- Default
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Adventure'], ['Arcane'], ['Eldrazi'], ['Elemental'], ['Elf'], ['Giant'], ['Goblin'], ['Merfolk'], ['Rogue']], NULL, NULL);
+
+-- Excludes non-null supertypes
+
+SELECT * FROM sorceries_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Adventure'], ['Arcane'], ['Eldrazi'], ['Elemental'], ['Elf'], ['Giant'], ['Goblin'], ['Merfolk'], ['Rogue']], 'Include', 'Legendary');
+
+-- Excludes null supertypes
+
 
 
 -- Query Type, Subtypes, Supertypes
