e0c605a50a9373196cb78a677c4fa602095af761
DaRoMi2020 <daniel.r.milman@gmail.com>
2020-07-17 14:12:21 -0700

    added rarity and color logic, minor type logic improvements, detailed comments on function options added

--- Artifacts_function.sql
+++ Artifacts_function.sql
@@ -1,14 +1,18 @@
-Artifact function
+-- Artifact function
 
 /**/
 
 
 CREATE FUNCTION artifacts_function (
-	artifacts_limit_v INTEGER, 
-	artifacts_rarity_v em_rarity,
+	artifacts_limit_v INTEGER,
 	format_v em_format, 
 	status_v em_status, 
-	color_v TEXT,
+	artifacts_rarity_floor em_rarity DEFAULT 'common',
+	artifacts_rarity_ceiling em_rarity DEFAULT 'mythic',
+	artifacts_colors_primary_exclude TEXT DEFAULT NULL,
+	artifacts_colors_primary_include TEXT [] DEFAULT array[['B'], ['G'], ['U'], ['W'], ['R']],
+	artifacts_colors_secondary TEXT [] DEFAULT NULL,
+	artifacts_colors_exclude_include TEXT DEFAULT 'Exclude',
 	artifacts_type_exclude TEXT DEFAULT NULL,
 	artifacts_type_include TEXT DEFAULT NULL,
 	artifacts_sub_exclude TEXT DEFAULT NULL,
@@ -36,41 +40,47 @@ RETURN QUERY
 
 WITH A AS (
 SELECT DISTINCT ON 
-(cards."name") cards."name", 
-cards.id, 
-cards.colors,
-cards.rarity,
-cards.types,
-cards.subtypes,
-cards.supertypes,
-legalities.format, 
-legalities.status 
+	(cards."name") cards."name", 
+	cards.id, 
+	cards.colors,
+	cards.rarity,
+	cards.types,
+	cards.subtypes,
+	cards.supertypes,
+	legalities.format, 
+	legalities.status 
 FROM cards 
 LEFT OUTER JOIN legalities 
 ON cards.uuid = legalities.uuid
 
 WHERE 
-((cards.colors::TEXT ILIKE color_v::TEXT) OR (cards.colors::TEXT IS NULL AND color_v::TEXT IS NULL))  AND
-cards.rarity = artifacts_rarity_v::em_rarity AND
-cards.types::TEXT NOT ILIKE '%Creature%' AND
-cards.types::TEXT NOT ILIKE '%Enchantment%' AND
-cards.types::TEXT NOT ILIKE '%Land%' AND
-cards.types::TEXT NOT ILIKE '%Hero%' AND
-legalities.format = format_v::em_format AND 
-legalities.status = status_v::em_status AND
+	legalities.format = format_v::em_format AND 
+	legalities.status = status_v::em_status AND
 
+	cards.rarity = ANY(enum_range(artifacts_rarity_floor::em_rarity, artifacts_rarity_ceiling::em_rarity)) AND
 
-((artifacts_type_exclude::TEXT IS NULL AND cards.types::TEXT ILIKE '%Artifact%' AND cards.types::TEXT ILIKE artifacts_type_include::TEXT IS NULL) OR -- Include or exclude tribal depending on subtype selection, 
-	(artifacts_type_exclude::TEXT IS NOT NULL AND cards.types::TEXT ILIKE 'Artifact' AND artifacts_type_include::TEXT IS NULL) OR -- Exclude tribal
-	(artifacts_type_exclude::TEXT IS NULL AND cards.types::TEXT ILIKE '%Artifact%' AND cards.types::TEXT ILIKE artifacts_type_include::TEXT)) AND -- only tribal
+	((artifacts_colors_primary_exclude::TEXT IS NULL AND cards.colors::TEXT IS NULL OR cards.colors::TEXT ~* ANY (artifacts_colors_primary_include::TEXT[])) OR -- include all choosen including null
+		(artifacts_colors_primary_exclude::TEXT IS NULL AND cards.colors::TEXT IS NULL AND artifacts_colors_primary_include::TEXT[] IS NULL) OR -- excludes non-null colors
+		(artifacts_colors_primary_exclude::TEXT IS NOT NULL AND cards.colors::TEXT IS NOT NULL AND cards.colors::TEXT ~* ANY (artifacts_colors_primary_include::TEXT[]))) AND -- exclude null
 
-((artifacts_sub_exclude::TEXT IS NULL AND cards.subtypes::TEXT IS NULL OR cards.subtypes::TEXT ~* ANY (artifacts_sub_include::TEXT[])) OR --include all choosen including null 
-	(artifacts_sub_exclude::TEXT IS NULL AND cards.subtypes::TEXT IS NULL AND artifacts_sub_include::TEXT[] IS NULL) OR --Exclude subtypes not null
-	(artifacts_sub_exclude::TEXT IS NOT NULL AND cards.subtypes::TEXT IS NOT NULL AND cards.subtypes::TEXT ~* ANY (artifacts_sub_include::TEXT[]))) AND --exclude nulls
+	((artifacts_colors_exclude_include::TEXT ILIKE 'Exclude' AND (artifacts_colors_secondary::TEXT[] IS NULL OR cards.colors::TEXT !~* ALL (artifacts_colors_secondary::TEXT[]))) OR
+		(artifacts_colors_exclude_include::TEXT ILIKE 'Include' AND (artifacts_colors_secondary::TEXT[] IS NULL OR cards.colors::TEXT ~* ANY (artifacts_colors_secondary::TEXT[])))) AND
 
-((artifacts_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL OR cards.supertypes::TEXT  ~* ANY (artifacts_super_include::TEXT[])) OR --include legendary including null
-	(artifacts_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL AND artifacts_super_include::TEXT[] IS NULL) OR --Exclude supertypes not null
-	(artifacts_super_exclude::TEXT IS NOT NULL AND cards.supertypes::TEXT IS NOT NULL AND cards.supertypes::TEXT  ~* ANY (artifacts_super_include::TEXT[]))) --include legendary and exclude nulls 
+	cards.types::TEXT NOT ILIKE '%Creature%' AND
+	cards.types::TEXT NOT ILIKE '%Enchantment%' AND
+	cards.types::TEXT NOT ILIKE '%Land%' AND
+
+	((artifacts_type_exclude::TEXT IS NULL AND cards.types::TEXT ILIKE '%Artifact%' AND cards.types::TEXT ILIKE artifacts_type_include::TEXT IS NULL) OR -- Include or exclude tribal depending on subtype selection, 
+		(artifacts_type_exclude::TEXT IS NOT NULL AND cards.types::TEXT ILIKE 'Artifact' AND artifacts_type_include::TEXT IS NULL) OR -- Exclude tribal
+		(artifacts_type_exclude::TEXT IS NOT NULL AND cards.types::TEXT ILIKE '%Artifact%' AND cards.types::TEXT ILIKE artifacts_type_include::TEXT)) AND -- only tribal
+
+	((artifacts_sub_exclude::TEXT IS NULL AND cards.subtypes::TEXT IS NULL OR cards.subtypes::TEXT ~* ANY (artifacts_sub_include::TEXT[])) OR --include all choosen including null 
+		(artifacts_sub_exclude::TEXT IS NULL AND cards.subtypes::TEXT IS NULL AND artifacts_sub_include::TEXT[] IS NULL) OR --Exclude subtypes not null
+		(artifacts_sub_exclude::TEXT IS NOT NULL AND cards.subtypes::TEXT IS NOT NULL AND cards.subtypes::TEXT ~* ANY (artifacts_sub_include::TEXT[]))) AND --exclude nulls
+
+	((artifacts_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL OR cards.supertypes::TEXT  ~* ANY (artifacts_super_include::TEXT[])) OR --include legendary including null
+		(artifacts_super_exclude::TEXT IS NULL AND cards.supertypes::TEXT IS NULL AND artifacts_super_include::TEXT[] IS NULL) OR --Exclude supertypes not null
+		(artifacts_super_exclude::TEXT IS NOT NULL AND cards.supertypes::TEXT IS NOT NULL AND cards.supertypes::TEXT  ~* ANY (artifacts_super_include::TEXT[]))) --include legendary and exclude nulls 
 
 ORDER BY (cards."name"))
 
@@ -84,36 +94,133 @@ END; $T$ LANGUAGE 'plpgsql';
 
 -- Function testing
 
-SELECT * FROM artifacts_function (1000, 'uncommon', 'legacy', 'Legal', NULL); 
 
-SELECT * FROM artifacts_function (1000, 'uncommon', 'legacy', 'Legal', NULL, NULL, NULL, NULL, array[['Equipment'],['Food'], ['Fortification'], ['Vehicle']], NULL, array[['Legendary'],['Snow']]);
+-- Function Testing
 
--- Includes all artifacts, includes all subtypes, includes all supertypes (default)
+-- Default
 
-SELECT * FROM artifacts_function (1000, 'uncommon', 'legacy', 'Legal', NULL, 'exclude'); 
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal');
 
--- Excludes tribal, includes all subtypes, includes all supertypes 
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic');
 
-SELECT * FROM artifacts_function (1000, 'uncommon', 'legacy', 'Legal', NULL, NULL, '%Tribal%'); 
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude');
 
--- Include only tribal artifact, includes only tribal subtypes, includes all supertypes 
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL);
 
-SELECT * FROM artifacts_function (1000, 'uncommon', 'legacy', 'Legal', NULL, NULL, NULL, 'include', array[['Equipment']]); 
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Equipment'],['Food'], ['Fortification'], ['Vehicle']]);
 
--- Includes all artifacts, includes only equipment, includes all supertypes 
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Equipment'],['Food'], ['Fortification'], ['Vehicle']], NULL, array[['Legendary'],['Snow']]);
 
-SELECT * FROM artifacts_function (1000, 'uncommon', 'legacy', 'Legal', NULL, NULL, NULL, NULL, NULL, NULL, NULL); 
+-- 1000 random cards (not a default option), legacy format (not a default option), legal in format(not a default option), 
+-- Rarities between common and mythic rare, includes all colored and null artifacts, excludes no colored and null artifacts
+-- Includes all types, include all subtypes, includes all supertypes
 
--- Excludes tribal type based on excluding subtype, excludes all subtypes, excludes all non-null supertypes 
+
+-- Rarity options
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic');
+
+-- Default 
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'uncommon', 'uncommon');
+
+-- Only includes artifacts that are uncommon
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'rare');
+
+-- Only includes artifacts with rarities that are between common and rare excludes mythic rares
+
+
+-- Exclusion mode
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude');
+
+-- Default
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G']], NULL, 'Exclude');
+
+-- Every card is either a black, green, or colorless, might have other colors as secondary colors.
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', 'Exclude Nulls', array[['B'], ['G']], NULL, 'Exclude');
+
+-- Every card is either a black or green but not colorless, might have other colors as secondary colors.
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', 'Exclude Nulls', array[['B'], ['G']], array[['W'], ['R'], ['U']], 'Exclude');
+
+-- Every card is either a black and/or green but not colorless, excludes white, red, and blue.
+
+-- Inclusion mode
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B']], array[['W']], 'Include');
+
+-- every card must contain black and white, might have other colors as secondary colors.
+
+
+-- Types Options
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL);
+
+-- Include all artifact types
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', 'Exclude', NULL);
+
+-- Exclude tribal artifact type
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', 'Include', '%Tribal%');
+
+-- Include only tribal atrifact type
+
+-- Subtypes
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Equipment'],['Food'], ['Fortification'], ['Vehicle']]);
+
+-- Default
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Equipment']]);
+
+-- Includes all null subtypes and selected subtypes
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, NULL);
+
+-- Excludes non-null subtypes (also excludes tribal type)
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	'Exclude', array[['Equipment'],['Food'], ['Fortification'], ['Vehicle']]);
+
+--Excludes null subtypes
+
+
+--Supertypes
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Equipment'],['Food'], ['Fortification'], ['Vehicle']], NULL, array[['Legendary'],['Snow']]);
+
+-- Default
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Equipment'],['Food'], ['Fortification'], ['Vehicle']], NULL, NULL);
+
+-- Excludes non-null supertypes
+
+SELECT * FROM artifacts_function (1000, 'legacy', 'Legal', 'common', 'mythic', NULL, array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude', NULL, NULL,
+	NULL, array[['Equipment'],['Food'], ['Fortification'], ['Vehicle']], 'Include', array[['Legendary'],['Snow']]);
+
+-- Excludes null supertypes
+
 
 
 -- Query Type, Subtypes, Supertypes
 
 SELECT DISTINCT ON (types) types, subtypes FROM cards WHERE types ILIKE '%Artifact%' 
-AND types NOT ILIKE '%Creature%' AND types NOT ILIKE '%Land%' AND types NOT ILIKE '%Enchantment%' AND types NOT ILIKE '%Hero%';
+AND types NOT ILIKE '%Creature%' AND types NOT ILIKE '%Land%' AND types NOT ILIKE '%Enchantment%';
 
 SELECT DISTINCT ON (subtypes) subtypes, types FROM cards WHERE types ILIKE '%Artifact%' 
-AND types NOT ILIKE '%Creature%' AND types NOT ILIKE '%Land%' AND types NOT ILIKE '%Enchantment%' AND types NOT ILIKE '%Hero%';
+AND types NOT ILIKE '%Creature%' AND types NOT ILIKE '%Land%' AND types NOT ILIKE '%Enchantment%';
 
 SELECT DISTINCT ON (supertypes) supertypes, types, subtypes FROM cards WHERE types ILIKE '%Artifact%';
 
