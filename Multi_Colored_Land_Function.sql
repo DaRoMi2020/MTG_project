71ad0f0570928aba27e4fc1fc4a54163eceb3ffb
DaRoMi2020 <daniel.r.milman@gmail.com>
2020-07-17 14:14:17 -0700

    added rarity and color logic, minor type logic improvements, detailed comments on function options added

--- Multi_Colored_Land_Function.sql
+++ Multi_Colored_Land_Function.sql
@@ -7,6 +7,8 @@ CREATE FUNCTION multi_color_land_function(
 	multi_color_land_limit_v INTEGER,
 	format_v em_format, 
 	status_v em_status,
+	planeswalkers_rarity_floor em_rarity DEFAULT 'common',
+	planeswalkers_rarity_ceiling em_rarity DEFAULT 'mythic',
 	mcl_identity_primary TEXT [] DEFAULT array[['B'], ['G'], ['U'], ['W'], ['R']],
 	mcl_identity_secondary TEXT [] DEFAULT NULL, 
 	mcl_identity_exclude_include TEXT DEFAULT 'Exclude')
@@ -51,10 +53,15 @@ WHERE
 	cards.layout != 'transform' AND
 	cards.types::TEXT NOT ILIKE '%Creature%' AND
 	cards.types::TEXT ILIKE '%Land%' AND
+
+	cards.rarity = ANY(enum_range(planeswalkers_rarity_floor::em_rarity, planeswalkers_rarity_ceiling::em_rarity)) AND
+
 	coloridentity::TEXT <> ALL (array[['B'], ['G'], ['U'], ['W'], ['R']]) AND
-	(mcl_identity_primary::TEXT[] ISNULL OR coloridentity::TEXT ~* ANY (mcl_identity_primary::TEXT[])) AND
-	((mcl_identity_exclude_include::TEXT ILIKE 'Exclude' AND (mcl_identity_secondary::TEXT[] ISNULL OR coloridentity::TEXT !~* ALL (mcl_identity_secondary::TEXT[]))) OR
-		(mcl_identity_exclude_include::TEXT ILIKE 'Include' AND (mcl_identity_secondary::TEXT[] ISNULL OR coloridentity::TEXT ~* ALL (mcl_identity_secondary::TEXT[]))))
+
+	(mcl_identity_primary::TEXT[] IS NULL OR coloridentity::TEXT ~* ANY (mcl_identity_primary::TEXT[])) AND
+
+	((mcl_identity_exclude_include::TEXT ILIKE 'Exclude' AND (mcl_identity_secondary::TEXT[] IS NULL OR coloridentity::TEXT !~* ALL (mcl_identity_secondary::TEXT[]))) OR
+		(mcl_identity_exclude_include::TEXT ILIKE 'Include' AND (mcl_identity_secondary::TEXT[] IS NULL OR coloridentity::TEXT ~* ALL (mcl_identity_secondary::TEXT[]))))
 
 ORDER BY (cards."name"))
 
@@ -71,68 +78,86 @@ END; $T$ LANGUAGE 'plpgsql';
 
 -- Function Testing
 
+
+-- Default
+
 -- Exclusion mode
 
 SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal');
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', NULL);
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal','common', 'mythic', NULL);
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', NULL, NULL);
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal','common', 'mythic', NULL, NULL);
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G'], ['U'], ['W'], ['R']]);
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G'], ['U'], ['W'], ['R']]);
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G'], ['U'], ['W'], ['R']], NULL);
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G'], ['U'], ['W'], ['R']], NULL);
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude');
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Exclude');
 
---Includes all multi-colored mana production, no exclusion. Default.
+--Includes all multi-colored mana production, no exclusion. 
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B']]);
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B']]);
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B']], NULL);
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B']], NULL);
 
 -- Every single land must have Black mana produciton, no exclusion
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G']]);
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G']]);
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G']], NULL);
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G']], NULL);
 
--- Every single land must have Black or Green mana produciton but not both only, no exclusion
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G']], NULL), 'Exclude';
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G']], array[['R']]);
+-- Every single land must have Black and/or Green mana produciton might have other colors, no exclusion
 
--- Every single land must have Black or Green mana produciton but not both only, excludes red mana production
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G']], array[['R']]);
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G']], array[['R'], ['W'], ['U']]);
+-- Every single land must have Black and/or Green mana produciton might have other other colors, excludes red mana production
+
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G']], array[['R'], ['W'], ['U']]);
 
 -- Every single land must have Black AND Green mana produciton only, excludes red, white, and blue mana production
 
------------------
 
 -- Inclusion mode
 
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', NULL, NULL, 'Include');
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', NULL, NULL, 'Include');
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Include');
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G'], ['U'], ['W'], ['R']], NULL, 'Include');
+-- Same as default of exclude mode
 
--- Same as default of exclude mode, include variable has no effect
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B']], NULL, 'Include');
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B']], NULL, 'Include');
+-- Every single land must have Black mana produciton, no required inclusion based on additional mana prouction
 
--- Every single land must have Black mana produciton, no required inclusion based on additional variable
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G']], NULL, 'Include');
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G']], NULL, 'Include');
+-- Every single land must have Black or Green mana produciton might have other colors, no required other inclusion
 
--- Every single land must have Black or Green mana produciton but not both only, no required inclusion based on additional variable
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G']], array[['R']], 'Include');
 
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G']], array[['R']], 'Include');
+-- Every single land must have Black or Green mana produciton, must include Red mana, might have other colors
 
--- Every single land must have Black or Green mana produciton but not both only, every single land must include Red mana
-
-SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', array[['B'], ['G']], array[['R'], ['W'], ['U']], 'Include');
+SELECT * FROM multi_color_land_function(1000, 'legacy', 'Legal', 'common', 'mythic', array[['B'], ['G']], array[['R'], ['W'], ['U']], 'Include');
 
 -- Selects a 5 color land
+
+
+-- Rarity options
+
+SELECT * FROM multi_color_land_function (1000, 'legacy', 'Legal', 'common', 'mythic');
+
+-- Default
+
+SELECT * FROM multi_color_land_function (1000, 'legacy', 'Legal', 'uncommon', 'uncommon'); 
+
+-- Only includes Non-basic lands that are uncommon
+
+SELECT * FROM multi_color_land_function (1000, 'legacy', 'Legal', 'common', 'rare'); 
+
+-- Only includes Non-basic lands  with rarities that are between common and rare excludes mythic rares
 
 
 
