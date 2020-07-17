edd2ef29a29f280f3d89ebae9946a19a686aba7c
DaRoMi2020 <daniel.r.milman@gmail.com>
2020-07-17 14:21:21 -0700

    added detailed comments on function

--- Basic_land_function.sql
+++ Basic_land_function.sql
@@ -1,10 +1,10 @@
 
---Land function
+-- Basic Land function
 
 /**/
 
 
-CREATE FUNCTION snow_basic_land_function ( 
+CREATE FUNCTION basic_land_function ( 
 	swamp_limit INTEGER DEFAULT 0,
 	forest_limit INTEGER DEFAULT 0,
 	island_limit INTEGER DEFAULT 0,
@@ -166,4 +166,140 @@ LIMIT colorless_limit::INTEGER);
 END; $T$ LANGUAGE 'plpgsql';
 
 
---SELECT * FROM snow_basic_land_function (500, 500, 500, 500, 500);
+-- Function Testing
+
+
+-- Default
+
+SELECT * FROM basic_land_function ();
+
+SELECT * FROM basic_land_function (0);
+
+SELECT * FROM basic_land_function (0, 0);
+
+SELECT * FROM basic_land_function (0, 0, 0);
+
+SELECT * FROM basic_land_function (0, 0, 0, 0);
+
+SELECT * FROM basic_land_function (0, 0, 0, 0, 0);
+
+SELECT * FROM basic_land_function (0, 0, 0, 0, 0, 0);
+
+SELECT * FROM basic_land_function (0, 0, 0, 0, 0, 0, 'Include');
+
+-- Include no basic lands, Include snow lands
+
+
+-- Swamps
+
+SELECT * FROM basic_land_function (10);
+
+SELECT * FROM basic_land_function (10, 0, 0, 0, 0, 0, 'Include');
+
+-- 10 basic swamps, Include snow lands
+
+SELECT * FROM basic_land_function (10, 0, 0, 0, 0, 0, 'Exclude');
+
+-- 10 basic swamps, Exclude snow lands
+
+SELECT * FROM basic_land_function (10, 0, 0, 0, 0, 0, 'Snow Only');
+
+-- 10 basic swamps, Exclude snow lands
+
+
+-- Forests
+
+SELECT * FROM basic_land_function (0, 10);
+
+SELECT * FROM basic_land_function (0, 10, 0, 0, 0, 0, 'Include');
+
+-- 10 basic forests, Include snow lands
+
+SELECT * FROM basic_land_function (0, 10, 0, 0, 0, 0, 'Exclude');
+
+-- 10 basic forests, Exclude snow lands
+
+SELECT * FROM basic_land_function (0, 10, 0, 0, 0, 0, 'Snow Only');
+
+-- 10 basic forests, Exclude snow lands
+
+
+-- Islands
+
+SELECT * FROM basic_land_function (0, 0, 10);
+
+SELECT * FROM basic_land_function (0, 0, 10, 0, 0, 0, 'Include');
+
+-- 10 basic islands, Include snow lands
+
+SELECT * FROM basic_land_function (0, 0, 10, 0, 0, 0, 'Exclude');
+
+-- 10 basic islands, Exclude snow lands
+
+SELECT * FROM basic_land_function (0, 0, 10, 0, 0, 0, 'Snow Only');
+
+-- 10 basic islands, Exclude snow lands
+
+
+-- Mountains 
+
+SELECT * FROM basic_land_function (0, 0, 0, 10);
+
+SELECT * FROM basic_land_function (0, 0, 0, 10, 0, 0, 'Include');
+
+-- 10 basic mountains, Include snow lands
+
+SELECT * FROM basic_land_function (0, 0, 0, 10, 0, 0, 'Exclude');
+
+-- 10 basic mountains, Exclude snow lands
+
+SELECT * FROM basic_land_function (0, 0, 0, 10, 0, 0, 'Snow Only');
+
+-- 10 basic mountains, Exclude snow lands
+
+
+-- Plains
+
+SELECT * FROM basic_land_function (0, 0, 0, 0, 10);
+
+SELECT * FROM basic_land_function (0, 0, 0, 0, 10, 0, 'Include');
+
+-- 10 basic plains, Include snow lands
+
+SELECT * FROM basic_land_function (0, 0, 0, 0, 10, 0, 'Exclude');
+
+-- 10 basic plains, Exclude snow lands
+
+SELECT * FROM basic_land_function (0, 0, 0, 0, 10, 0, 'Snow Only');
+
+-- 10 basic plains, Exclude snow lands
+
+
+-- Colorless
+
+SELECT * FROM basic_land_function (0, 0, 0, 0, 0, 10);
+
+SELECT * FROM basic_land_function (0, 0, 0, 0, 0, 10, 'Include');
+
+-- 10 basic colorless, Include snow lands (no colorless snow lands)
+
+
+-- mix of lands
+
+SELECT * FROM basic_land_function (10, 10, 10, 10, 10, 10);
+
+SELECT * FROM basic_land_function (10, 10, 10, 10, 10, 10, 'Include');
+
+-- 10 of each basic land type, Include snow lands
+
+SELECT * FROM basic_land_function (10, 10, 10, 10, 10, 10, 'Exclude');
+
+-- 10 of each basic land type, Exclude snow lands
+
+SELECT * FROM basic_land_function (10, 10, 10, 10, 10, 0, 'Snow Only');
+
+-- 10 of each basic land type except colorless, Only snow lands
+
+
+
+
